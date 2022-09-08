6443fe1e742890325d31c1ee59caaeb23a68a1ca
pimiralles10 <112995321+pimiralles10@users.noreply.github.com>
2022-09-08 11:01:20 +0800

    dbtest

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 
 @SpringBootApplication
 @RestController
@@ -17,4 +23,22 @@ public class DemoApplication {
 	String sayHello() {
 		return "Hello World!";
 	}
+}
+
+public class SimpleTesting{
+    public static void main(String[] args) {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://10.2.0.4:3306/zditectDB","DBConnect","demo@user123");
+            Statement stmt=con.createStatement();  
+            ResultSet rs=stmt.executeQuery("show databases;");
+            System.out.println("Connected");  
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }  
 }
