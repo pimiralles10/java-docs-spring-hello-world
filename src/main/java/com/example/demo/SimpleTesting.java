5e7ab8708bc23062313e7f1b3eeea2c9cd503ba4
pimiralles10 <112995321+pimiralles10@users.noreply.github.com>
2022-09-08 11:37:28 +0800

    simpletest2

--- src/main/java/com/example/demo/SimpleTesting.java
+++ src/main/java/com/example/demo/SimpleTesting.java
@@ -9,7 +9,7 @@ public class SimpleTesting{
         {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://10.2.0.4:3306/zditectDB","DBConnect","demo@user123");
+                    "jdbc:mysql://10.2.0.4:3306","DBConnect","demo@user123");
             Statement stmt=con.createStatement();  
             ResultSet rs=stmt.executeQuery("show databases;");
             System.out.println("Connected");  
