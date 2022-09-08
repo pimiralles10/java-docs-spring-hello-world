71d40ccabac6a994b56f0ba36a482763ac1370d9
pimiralles10 <112995321+pimiralles10@users.noreply.github.com>
2022-09-08 11:49:48 +0800

    simpletestpamore

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 
@@ -22,4 +25,22 @@ public class DemoApplication {
 		return "my Database Testing";
     
 	}
+    
+    public static void main(String[] args) {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://10.2.0.4:3306","DBConnect","demo@user123");
+            Statement stmt=con.createStatement();  
+            ResultSet rs=stmt.executeQuery("show databases;");
+            System.out.println("Connected");  
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }  
 }
+
+
